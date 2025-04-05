873a09c790051c9749b3adaca47d42940581d718
nadialvy <5026231007@student.its.ac.id>
2025-04-06 05:25:55 +0700

    feat: responsive and add sound cam snap when taking photos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -56,6 +56,8 @@ export default function Home() {
 
     const context = canvas.getContext("2d");
     if (!context) return;
+    const audio = new Audio("/audios/cam_snap.mp3");
+    audio.play();
 
     // Apply filter
     context.filter = filterMap[selectedFilter];
@@ -105,19 +107,15 @@ export default function Home() {
   };
 
   return (
-    <div className="w-full p-6 flex flex-col justify-center items-center bg-gradientCloud bg-cover bg-center bg-no-repeat h-screen font-[family-name:var(--font-lilita-one)]">
-      <div className="inset-0 absolute bg-black bg-opacity-5 z-0 h-screen w-full">
-        {}
-      </div>
-
+    <div className="max-w-full p-6 flex flex-col justify-center items-center bg-gradientCloud bg-cover bg-center bg-no-repeat min-h-screen font-[family-name:var(--font-lilita-one)]">
       <p
-        className="text-[48px] text-[#fcd3d2] drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]
+        className="text-[48px] max-md:text-[30px] text-center text-[#fcd3d2] drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]
 "
       >
         Self Photobooth
       </p>
-      <div className="w-full z-10 h-full flex justify-center items-center">
-        <div className="w-1/2 h-full relative">
+      <div className="w-full flex-col lg:flex-row min-w-fit z-10 h-full flex justify-center items-center">
+        <div className="lg:w-1/2 w-full h-full relative">
           <div className="relative w-full h-full flex justify-center items-center">
             <Image
               src="/images/frame.png"
@@ -126,10 +124,12 @@ export default function Home() {
               height={500}
               className="w-full h-full z-10"
             />
-            <CameraPreview
-              filter={selectedFilter}
-              className="absolute top-[13%] z-0 left-[13%] w-[450px] h-[430px]"
-            />
+            <div className="w-[70%] h-[70%] absolute left-[15%] top-[18%] z-0">
+              <CameraPreview
+                filter={selectedFilter}
+                className="absolute top-0 z-0 left-0 w-full h-full"
+              />
+            </div>
           </div>
           <Image
             onClick={capturePhoto}
@@ -137,7 +137,7 @@ export default function Home() {
             alt="button"
             width={240}
             height={240}
-            className="absolute hover:cursor-pointer hover:brightness-75 transition-all duration-200 z-50 bottom-[22%] left-[50%] translate-x-[-50%] translate-y-[50%] object-cover"
+            className="absolute max-md:w-32 hover:cursor-pointer hover:brightness-75 transition-all duration-200 z-50 bottom-[22%] left-[50%] translate-x-[-50%] translate-y-[50%] object-cover"
           />
           <div className="absolute top-0 z-0 right-0 w-[450px] h-[430px] flex justify-center items-center">
             {countdownDisplay && (
@@ -147,16 +147,16 @@ export default function Home() {
             )}
           </div>
         </div>
-        <div className="w-1/2 gap-4 bg-gradientSettings border-[8px]  border-[#efb4e1] bg-cover bg-center bg-no-repeat rounded-3xl p-6 h-full flex flex-col justify-start items-start">
+        <div className="lg:w-1/2 w-full gap-4 bg-gradientSettings border-[8px]  border-[#efb4e1] bg-cover bg-center bg-no-repeat rounded-3xl p-6 h-full flex flex-col justify-start items-start">
           <div>
-            <p className="text-[28px] text-[#8f73d1] drop-shadow-[0_1.2px_1.2px_#ffe1cd]">
+            <p className="text-[28px] max-md:text-[22px] text-[#8f73d1] drop-shadow-[0_1.2px_1.2px_#ffe1cd]">
               Photo Preview
             </p>
-            <div className="flex gap-3 mt-3 justify-start items-start">
+            <div className="flex gap-3 mt-3 justify-start max-md:flex-wrap items-start">
               {Array.from({ length: numberPhotos }, (_, idx) => (
                 <div
                   key={idx}
-                  className="bg-[#ffdada] border-2 border-[#efb4e1] w-[120px] h-[120px] rounded-lg overflow-hidden"
+                  className="bg-[#ffdada] border-2 border-[#efb4e1] w-[60px] h-[60px] md:w-[100px] md:h-[100px] lg:w-[120px] lg:h-[120px] rounded-lg overflow-hidden"
                 >
                   {capturedPhotos[idx] && (
                     <div className="relative w-full h-full">
@@ -183,9 +183,9 @@ export default function Home() {
               ))}
             </div>
           </div>
-          <div className="flex gap-4 w-full justify-center items-start">
-            <div className="w-1/2">
-              <p className="text-[28px] text-[#8f73d1] drop-shadow-[0_1.2px_1.2px_#ffe1cd]">
+          <div className="flex md:flex-row flex-col gap-4 w-full justify-center items-start">
+            <div className="w-full md:w-1/2">
+              <p className="text-[28px] max-md:text-[22px] text-[#8f73d1] drop-shadow-[0_1.2px_1.2px_#ffe1cd]">
                 Filter
               </p>
               <div className="flex flex-col gap-2 items-center justify-center">
@@ -232,13 +232,15 @@ export default function Home() {
                 </div>
               </div>
             </div>
-            <div className="w-1/2">
-              <p className="text-[28px] text-[#8f73d1] ">Options</p>
+            <div className="w-full md:w-1/2">
+              <p className="text-[28px] text-[#8f73d1] max-md:text-[22px]">
+                Options
+              </p>
               <div>
-                <p className="text-[#8f73d1] font-quicksand font-bold text-[20px]">
+                <p className="text-[#8f73d1] max-md:text-[16px] font-quicksand font-bold text-[20px]">
                   Number of Photos
                 </p>
-                <div className="flex bg-[#fddeea] border-4 border-[#efb4e1] px-4 py-2 rounded-full gap-2 justify-between items-center">
+                <div className="flex bg-[#fddeea] border-4 border-[#efb4e1] px-4 py-2 max-md:py-0 rounded-full gap-2 justify-between items-center">
                   <p
                     onClick={decreasePhotos}
                     className="hover:cursor-pointer text-[#674f9e] text-[20px]"
@@ -255,10 +257,10 @@ export default function Home() {
                 </div>
               </div>
               <div className="">
-                <p className="text-[#8f73d1] font-quicksand font-bold text-[20px]">
+                <p className="text-[#8f73d1] font-quicksand mt-2 max-md:text-[16px] font-bold text-[20px]">
                   Timer
                 </p>
-                <div className="flex bg-[#fddeea] border-4 border-[#efb4e1] px-4 py-2 rounded-full gap-2 justify-between items-center">
+                <div className="flex bg-[#fddeea] border-4 border-[#efb4e1] max-md:py-0 px-4 py-2 rounded-full gap-2 justify-between items-center">
                   <select
                     className="bg-transparent w-full text-[#674f9e] text-[20px] outline-none"
                     value={timer}
